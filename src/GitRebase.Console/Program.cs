6ad316bd602d34b6c9cf70029e044ccb8abb8ce6
Wilco Boshoff <wlboshoff@gmail.com>
2024-03-23 21:47:50 +0200

    Initial commit

---
+++ src/GitRebase.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+
+IHost host = builder.Build();
+
+Console.WriteLine($"Hello! Learning Git Rebase I see 😀{Environment.NewLine}");
+
+await host.RunAsync();
