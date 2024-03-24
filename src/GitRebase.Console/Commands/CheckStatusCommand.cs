8421e65d3757d1d311120ffb0d9642efbf8774ba
Wilco Boshoff <wlboshoff@gmail.com>
2024-03-24 12:48:58 +0200

    Add Status Check command

---
+++ src/GitRebase.Console/Commands/CheckStatusCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+using HealthChecks.UI.Core;
+
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace GitRebase.Commands;
+
+internal class CheckStatusCommand : Command
+{
+    private readonly HealthCheckService _healthCheckService;
+
+    public CheckStatusCommand(HealthCheckService healthCheckService)
+    {
+        Key = "status";
+        _healthCheckService = healthCheckService;
+    }
+
+    public override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Checking health status...");
+
+        HealthReport healthReport = await _healthCheckService.CheckHealthAsync(cancellationToken);
+        UIHealthReport report = UIHealthReport.CreateFrom(healthReport);
+
+        Console.WriteLine($"Health status is {JsonSerializer.Serialize(report)}");
+    }
+}
