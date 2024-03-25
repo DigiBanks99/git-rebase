59662f7120ec721c2413da59ee3d50735c12964c
Wilco Boshoff <wlboshoff@gmail.com>
2024-03-25 06:16:21 +0200

    Add help command

---
+++ src/GitRebase.Console/Commands/PrintHelpCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace GitRebase.Commands;
+
+internal class PrintHelpCommand : Command
+{
+    public PrintHelpCommand()
+    {
+        Key = "help";
+    }
+
+    public override Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine(
+            @"This is a simple console app that takes in a command and produces a certain output to the console
+
+Commands:
+\thello - Prints a welcoming message
+\thelp - Prints this help message");
+        return Task.CompletedTask;
+    }
+}
